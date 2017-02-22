be2674af90b0bb199fcc2e7735df289d9485f613
Arun Kumar <arunk.s@outlook.com>
2017-02-22 15:40:54 +0530

    added jquery and scss loader

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+//Styles
 import './styles/bootstrap.min.css';
-import './styles/style.css';
+import './styles/style.scss';
+
+//Scripts
+import 'jquery';
 import './scripts/bootstrap.min.js';
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
 var path = require('path');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var webpack = require('webpack');
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'dist.js',
     path: path.resolve(__dirname, 'dist')
   },
+  devtool: 'source-map',
   module: {
     loaders: [{
       test: /\.js$/,
@@ -14,12 +16,17 @@ module.exports = {
       query: {
         presets: [['es2015', {modules: false}]],
         plugins: ['syntax-dynamic-import']
-      }
-	    },
-	    { test: /\.css$/, loader: ExtractTextPlugin.extract({
-	        use: 'css-loader'
-	     }) },
-	    {
+      	}
+	  },
+	  { test: /\.css$/, 
+	    loader: ExtractTextPlugin.extract({
+	        	use: 'css-loader'
+	     }) 
+	  },
+	  { test: /\.scss$/,
+	  	loader: ExtractTextPlugin.extract('css-loader!sass-loader')
+	  },
+	  {
 	    test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
 	    loader: 'url-loader?limit=10000&mimetype=application/font-woff'
 	  },
@@ -30,6 +37,10 @@ module.exports = {
   	]
   },
   plugins:[
-    	new ExtractTextPlugin('styles.css')
+    	new ExtractTextPlugin('styles.css'),
+    	new webpack.ProvidePlugin({
+           $: "jquery",
+           jQuery: "jquery"
+       })
   ]
 };
