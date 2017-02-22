5d3931a0ebb7fa3c51d511edb1d1040a5d7f54b8
Arun Kumar <Arunku@Etechaces.com>
2017-02-22 13:08:43 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import './styles/bootstrap.min.css';
+import './styles/style.css';
+import './scripts/bootstrap.min.js';
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'dist.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /(node_modules)/,
+      loader: 'babel-loader',
+      query: {
+        presets: [['es2015', {modules: false}]],
+        plugins: ['syntax-dynamic-import']
+      }
+    },
+    { test: /\.css$/, loader: ExtractTextPlugin.extract({
+        use: 'css-loader'
+     }) },
+    {
+    test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+    loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+  },
+  {
+    test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+    loader: 'file-loader'
+  }
+  ]
+  },
+  plugins:[
+    	new ExtractTextPlugin('styles.css')
+  ]
+};
