c9edf81d84283b6fd80c14367de13d589b878c87
Arun Kumar <arunk.s@outlook.com>
2017-02-22 13:22:37 +0530

    indentation

--- webpack.config.js
+++ webpack.config.js
@@ -15,19 +15,19 @@ module.exports = {
         presets: [['es2015', {modules: false}]],
         plugins: ['syntax-dynamic-import']
       }
-    },
-    { test: /\.css$/, loader: ExtractTextPlugin.extract({
-        use: 'css-loader'
-     }) },
-    {
-    test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-    loader: 'url-loader?limit=10000&mimetype=application/font-woff'
-  },
-  {
-    test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-    loader: 'file-loader'
-  }
-  ]
+	    },
+	    { test: /\.css$/, loader: ExtractTextPlugin.extract({
+	        use: 'css-loader'
+	     }) },
+	    {
+	    test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+	    loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+	  },
+	  {
+	    test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+	    loader: 'file-loader'
+	  }
+  	]
   },
   plugins:[
     	new ExtractTextPlugin('styles.css')
